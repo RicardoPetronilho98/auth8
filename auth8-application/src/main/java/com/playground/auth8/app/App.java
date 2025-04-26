c1b8a276f02906465bb425fb2b4cd7baf521d818
Ricardo Petronilho <rpetronilho98@gmail.com>
2025-04-26 15:41:05 +0100

    Removed logging library from this repo. Created a new repo with the library.

--- auth8-application/src/main/java/com/playground/auth8/app/App.java
+++ auth8-application/src/main/java/com/playground/auth8/app/App.java
@@ -3,7 +3,7 @@ package com.playground.auth8.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.playground.auth8")
+@SpringBootApplication(scanBasePackages = "com.playground")
 public class App {
 
     public static void main(String[] args) {
